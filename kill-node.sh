506ae8fe371b6cf8411fe0e47fcf467254d74a45
Lycolia <lycolia@lycolia.info>
2020-09-22 20:46:46 +0900

    bugfix and add echo color setting

--- kill-node.sh
+++ kill-node.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
+# echo color
+## infomation
+INF=$(printf '\033')"[36m"
+## error
+ERR=$(printf '\033')"[31m"
+## color syntax end
+CLR=$(printf '\033')"[m"
+
+# kill exit status
+# 0: success, 1: failure
+result=1
 # find node process id
 node_prc=`lsof -i -n | grep 9229 | awk '{print $2}'`
-result="-1"
+
 
 if [ ${#node_prc} -gt 0 ]; then
     # node_prc.length > 0
-    echo "found node process"
+    echo -e "${INF}found node process${CLR}"
     kill -9 "${node_prc}"
     # get kill exit status
     result=$?
-    if [ result = "0" ]; then
+    if [ ${result} -eq 0 ]; then
         # success
-        echo "killed node process ${node_prc}"
+        echo -e "${INF}killed node process ${node_prc}${CLR}"
     else
         # failure
-        echo "failed kill node process ${node_prc}"
-        echo "kill exit status ${result}"
+        echo -e "${ERR}failed kill node process ${node_prc}${CLR}"
+        echo -e "${ERR}kill exit status ${result}${CLR}"
     fi
 else
     # node_prc.length === 0
-    echo "node process not found"
+    echo -e "${INF}node process not found${CLR}"
 fi
